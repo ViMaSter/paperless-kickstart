24be9ea3bd85988a4c72bb8bcaef9936af7f6107
Vincent Mahnke <vincent@mahn.ke>
2026-02-01 19:32:45 +0100

    fix: Replaces hardcoded username

--- start.bat
+++ start.bat
@@ -2,12 +2,12 @@
 setlocal EnableDelayedExpansion
 
 cd paperless
-docker compose -p paperless_daniel up -d
+docker compose -p "paperless_%USERNAME%" up -d
 
 echo Waiting for Paperless to start... (This might take 2-5 minutes initially.)
 set attempt=1
 :wait
-for /f "usebackq delims=" %%J in (`docker ps --filter "name=paperless_daniel-webserver-1" --format json`) do (
+for /f "usebackq delims=" %%J in (`docker ps --filter "name=paperless_%USERNAME%-webserver-1" --format json`) do (
     set "JSON=%%J"
 	echo %JSON%
     echo %JSON% | findstr /i "healthy" >nul
